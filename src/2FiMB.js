bb1cfc4a1616a71dbc3cc6427fbc2c6d13121bc8
Wizzy <82947033+Wizzy-TV@users.noreply.github.com>
2024-04-20 13:06:05 +0200

    Update 2FiMB.js

--- src/2FiMB.js
+++ src/2FiMB.js
@@ -1,1 +1,8 @@
-
+/** 
+ * Name: 2FiMB
+ * Version: 1.0.0
+ * Description: 2FiMB (Force Fullscreen in Mobile Browser) - Force your Mobile Browser to Access your favorite website in Fullscreen Mode!
+ * Author: Wizzy
+ * Author Website: https://iam-wizzy.web.app
+ * License: MIT
+ */
