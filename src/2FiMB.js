bc485c6b5ea496ec18f5f049cdf1e59039df776a
Wizzy <82947033+Wizzy-TV@users.noreply.github.com>
2024-04-21 11:01:59 +0200

    Update 2FiMB.js

--- src/2FiMB.js
+++ src/2FiMB.js
@@ -8,7 +8,7 @@
  */
 const FiMBbtn = document.createElement('button');
 FiMBbtn.innerHTML = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-arrows-fullscreen" viewBox="0 0 16 16">
+    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="rgba(255, 255, 255, 0.9)" class="bi bi-arrows-fullscreen" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707m4.344 0a.5.5 0 0 1 .707 0l4.096 4.096V11.5a.5.5 0 1 1 1 0v3.975a.5.5 0 0 1-.5.5H11.5a.5.5 0 0 1 0-1h2.768l-4.096-4.096a.5.5 0 0 1 0-.707m0-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707m-4.344 0a.5.5 0 0 1-.707 0L1.025 1.732V4.5a.5.5 0 0 1-1 0V.525a.5.5 0 0 1 .5-.5H4.5a.5.5 0 0 1 0 1H1.732l4.096 4.096a.5.5 0 0 1 0 .707"/>
     </svg>
 `;
@@ -18,8 +18,10 @@ FiMBbtn.querySelector('svg').style.display = 'block';
 FiMBbtn.style.position = 'fixed';
 FiMBbtn.style.zIndex = '9999';
 FiMBbtn.style.border = 'none';
-FiMBbtn.style.padding = '0';
+FiMBbtn.style.borderRadius = '2px';
+FiMBbtn.style.padding = '8px';
 FiMBbtn.style.margin = '0';
+FiMBbtn.style.background = 'rgba(0, 0, 0, 0.4)';
 FiMBbtn.style.right = '20px';
 FiMBbtn.style.bottom = '20px';
 
